ce346d6bb7d35bb25aab58157bc67d66c5e95e08
João Pedro Barbosa <85754689+jotape-b@users.noreply.github.com>
2024-12-12 23:56:11 +0000

    Criar guia de UI em Main

--- Trabalho1/app/Main.java
+++ Trabalho1/app/Main.java
@@ -2,5 +2,27 @@ package app;
 public class Main{
     public static void main(String[] args){
         System.out.println("Hello world");
+        /*
+                • Menus e submenus a serem criados:
+        ◦ Para ambos os usuários (primeiro menu apresentado):
+        ▪ Login
+        • Solicitar usuário e senha e realizar a validação.
+        • O sistema deve ser entregue com um único usuário “admin” (com senha “admin”)
+        criado.
+        ◦ Para usuários administradores:
+        ▪ Create new product
+        ▪ Create new user
+        • Deverá solicitar se o novo usuário sendo criado é um cliente ou um
+        administrador, seu login e sua senha.
+        ▪ Report – more expensive order
+        ▪ Report – product with lowest inventory
+        ▪ Exit
+        ◦ Para usuários clientes:
+        ▪ Start new order
+        • Add product
+        • View shopping cart
+        ◦ Listar todos os produtos incluídos no carrinho de compras
+        • Finish order
+        ▪ Exit */
     }
 }
